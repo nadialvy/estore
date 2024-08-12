a97f73f65c7c158bea7c4654b3c8f7a8f75f35a2
nadialvy <5026231007@student.its.ac.id>
2024-08-12 12:15:19 +0700

    Initial Commit

---
+++ estore/estoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  estoreApp.swift
+//  estore
+//
+//  Created by Nadia Lovely on 12/08/24.
+//
+
+import SwiftUI
+
+@main
+struct estoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
