a5598519230e632031d11faf440fb5bff024703e
nadialvy <5026231007@student.its.ac.id>
2024-08-14 17:27:51 +0700

    feat: add confirm dialog on delete user

--- estore/PageViews/Users.swift
+++ estore/PageViews/Users.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct Users: View {
     @StateObject private var userVM = UserVM()
     @Environment(\.editMode) private var editMode
+    @State private var showDeleteConfirmation = false
+    @State private var deleteOffsets: IndexSet?
     
     var body: some View {
         NavigationStack{
@@ -17,25 +19,41 @@ struct Users: View {
                 ForEach(userVM.users, id: \.id){ user in
                     UserCard(userData: user)
                 }
-                .onDelete(perform: delete)
+                .onDelete(perform: confirmDelete)
             }
             .navigationTitle("Users")
-            .toolbar{
-                ToolbarItem{
+            .toolbar {
+                ToolbarItem {
                     EditButton()
-                    .foregroundColor(.red)
+                        .foregroundColor(.red)
                 }
             }
+            .confirmationDialog("Are you sure you want to delete this item?", isPresented: $showDeleteConfirmation) {
+                Button("Delete", role: .destructive) {
+                    if let offsets = deleteOffsets {
+                        delete(offsets: offsets)
+                    }
+                }
+                Button("Cancel", role: .cancel) {
+                    deleteOffsets = nil
+                }
+            }
+            
             .task{
                 await userVM.loadUsers()
             }
         }
     }
     
+    private func confirmDelete(offsets: IndexSet) {
+        deleteOffsets = offsets
+        showDeleteConfirmation = true
+    }
+    
     private func delete(offsets: IndexSet) {
         userVM.users.remove(atOffsets: offsets)
-    }
-}
+        deleteOffsets = nil
+    }}
 
 #Preview {
     Users()
